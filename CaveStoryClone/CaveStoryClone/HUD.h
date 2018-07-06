1f8e28ac3fd0ff025d08f1377a9d44b6a9047e0f
Will Stern <wlbs523@gmail.com>
2018-07-06 15:55:09 +0100

    Added additional comments
    added a few extra comments to help find and select sections of the textbox sprite sheet.

--- CaveStoryClone/CaveStoryClone/HUD.h
+++ CaveStoryClone/CaveStoryClone/HUD.h
@@ -32,4 +32,12 @@ private:
 	Sprite _slash;
 	Sprite _dashes;
 
+	//potential additions (all found in the Textbox.png)
+	//ESC to Quit text:			x = 3, y = 132, w = 49, h = 9
+	//Yes / No Select Textbox:	x = 152, y = 48, w = 84, h = 32
+	//Light Select Hand:		x = 113, y = 90, w = 13, h = 10
+	//Darker Select Hand:		x = 129, y = 90, w = 13, h = 10
+	//Air Text:					x = 121, y = 80, w = 23, h = 8
+	//light exp bar:			x = 0, y = 33, w = 231, h = 5
+	//Textbox background:		x = 0, y = 0, w = 244, h = 24
 };
